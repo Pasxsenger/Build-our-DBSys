25838803a002c518bd1a2b658fc9aea795167bd7
Xuesong Shen <christophershen0121@gmail.com>
2023-11-15 01:34:19 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-# from CodeForSQL.sql_handler import SQLHandler
+from CodeForSQL.sql_handler import SQLHandler
 # from CodeForNoSQL.nosql_handler import NoSQLHandler
+
 
 def main():
     print("Welcome to ourDB. Please choose 'SQL' or 'NoSQL'. Type 'exit' to quit.")
@@ -10,15 +11,15 @@ def main():
             print("Exiting ourDB. Goodbye!")
             break
         elif db_type.lower() == 'sql':
-            print("sql")
-            # sql_handler = SQLHandler()
-            # sql_handler.handle_commands()
+            sql_handler = SQLHandler()
+            sql_handler.handle_commands()
         elif db_type.lower() == 'nosql':
             print("nosql")
             # nosql_handler = NoSQLHandler()
             # nosql_handler.handle_commands()
         else:
             print("Invalid input. Please choose 'SQL' or 'NoSQL'.")
+
 
 if __name__ == "__main__":
     main()
