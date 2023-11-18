adeea55629e95723d7d994b2b044acfdb5787288
Xuesong Shen <christophershen0121@gmail.com>
2023-11-17 20:17:28 -0800

    Update sql_handler.py

--- Code/CodeForSQL/sql_handler.py
+++ Code/CodeForSQL/sql_handler.py
@@ -1,4 +1,6 @@
 import importlib
+import os
+import shutil
 
 
 class SQLHandler:
@@ -11,8 +13,39 @@ class SQLHandler:
         while True:
             line = input("SQL_DB > " if not command else "... ")
             command += line
+            # if command.lower().strip() == 'exit':
+            #     # Check if second-lowest path is 'SQL_DB'
+            #     if os.path.basename(os.path.dirname(self.db_path)) == "SQL_DB":
+            #         running_directory = os.path.join(self.db_path, f"{os.path.basename(self.db_path)}_running")
+            #         # Delete the running_directory if it exists
+            #         if os.path.exists(running_directory):
+            #             shutil.rmtree(running_directory)
+            #             # Create a new running_directory
+            #             os.makedirs(running_directory)
+            #     return
+
             if command.lower().strip() == 'exit':
+                # Identify SQL_DB directory
+                current_path = self.db_path
+                while os.path.basename(current_path) != "SQL_DB" and os.path.basename(os.path.dirname(current_path)) != "SQL_DB":
+                    current_path = os.path.dirname(current_path)
+
+                if os.path.basename(current_path) == "SQL_DB":
+                    sql_db_directory = current_path
+                else:
+                    sql_db_directory = os.path.dirname(current_path)
+
+                # Iterate through each subdirectory in SQL_DB
+                for subdir in os.listdir(sql_db_directory):
+                    subdir_path = os.path.join(sql_db_directory, subdir)
+                    running_directory = os.path.join(subdir_path, f"{subdir}_running")
+                    if os.path.isdir(subdir_path) and os.path.exists(running_directory):
+                        # Delete the running_directory if it exists
+                        shutil.rmtree(running_directory)
+                        # Optionally, recreate the running_directory
+                        os.makedirs(running_directory)
                 return
+
             if command.endswith(';'):
                 command = command[:-1].strip()  # Remove the semicolon and strip whitespace
                 self.process_command(command)
